2b1a8b65d7ff072c637fdccbe9f1b5c4c50a7279
VaighundaSubash <35206935+VaighundaSubash@users.noreply.github.com>
2018-11-01 22:56:31 +0530

    Add files via upload

---
+++ PythonUrlApplication/PythonUrlApplication/PythonUrlApplication.py
@@ -0,0 +1,23 @@
+import urllib.request;
+
+import re;
+
+url = input("Please enter the Valid URL : ");
+
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE);
+
+if re.match(regex, url):
+    request = urllib.request.urlopen(url);
+    
+    html = request.read();
+    #print(html);
+    print("Status code = ", request.getcode());
+    
+else:
+    print("Please enter the valid URL!!!");
